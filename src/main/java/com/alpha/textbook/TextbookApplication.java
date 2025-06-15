e5b03cd5dd362ff523169b78e83bcd25e0a239d0
baiman <baiman@baiman.delilah>
2025-06-16 03:53:13 +0545

    initial push for textbook

---
+++ src/main/java/com/alpha/textbook/TextbookApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.textbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/textbook/TextbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.textbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
